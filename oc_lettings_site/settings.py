83860631093317e994631d67bd0e97a9a6571c93
Mimi <hana.nguyen@outlook.fr>
2024-07-24 22:44:41 +0200

    Updating settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -15,8 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True # Set on true for static files and false to test error pages
 ALLOWED_HOSTS = ["127.0.0.1"]
 
 
