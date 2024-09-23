0fdeabd857e8f545ac3366e0804ccf15c22974b3
Mimi <hana.nguyen@outlook.fr>
2024-09-23 15:38:20 +0200

    Adding dockerhub

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Set on true for static files and false to test error pages
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'python-oc-lettings-fr-qgyr.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
