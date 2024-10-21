9d5e784d90984f0d1859fc0c6187c7cdd518aa65
Mimi <hana.nguyen@outlook.fr>
2024-10-21 21:30:41 +0200

    Adding sentry try/except

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from lettings.models import Letting
 from profiles.models import Profile
+import sentry_sdk
 
 
 def index(request):
@@ -13,7 +14,11 @@ def index(request):
     Returns:
         HttpResponse: Rendered homepage.
     """
-    return render(request, "index.html")
+    try:
+        return render(request, "index.html")
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
 
 
 def lettings_index(request):
@@ -26,9 +31,13 @@ def lettings_index(request):
     Returns:
         HttpResponse: Rendered page with a list of all lettings.
     """
-    lettings_list = Letting.objects.all()
-    context = {"lettings_list": lettings_list}
-    return render(request, "lettings/index.html", context)
+    try:
+        lettings_list = Letting.objects.all()
+        context = {"lettings_list": lettings_list}
+        return render(request, "lettings/index.html", context)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
 
 
 def letting(request, letting_id):
@@ -42,12 +51,18 @@ def letting(request, letting_id):
     Returns:
         HttpResponse: Rendered page with details of the specified letting.
     """
-    letting = Letting.objects.get(id=letting_id)
-    context = {
-        "title": letting.title,
-        "address": letting.address,
-    }
-    return render(request, "lettings/letting.html", context)
+    try:
+        letting = Letting.objects.get(id=letting_id)
+        context = {
+            "title": letting.title,
+            "address": letting.address,
+        }
+        return render(request, "lettings/letting.html", context)
+    except Letting.DoesNotExist:
+        return render(request, "errors/error_404.html", status=404)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
 
 
 def profiles_index(request):
@@ -60,9 +75,13 @@ def profiles_index(request):
     Returns:
         HttpResponse: Rendered page with a list of all user profiles.
     """
-    profiles_list = Profile.objects.all()
-    context = {"profiles_list": profiles_list}
-    return render(request, "profiles/index.html", context)
+    try:
+        profiles_list = Profile.objects.all()
+        context = {"profiles_list": profiles_list}
+        return render(request, "profiles/index.html", context)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
 
 
 def profile(request, username):
@@ -76,18 +95,25 @@ def profile(request, username):
     Returns:
         HttpResponse: Rendered page with the specified user profile.
     """
-    profile = Profile.objects.get(user__username=username)
-    context = {"profile": profile}
-    return render(request, "profiles/profile.html", context)
+    try:
+        profile = Profile.objects.get(user__username=username)
+        context = {"profile": profile}
+        return render(request, "profiles/profile.html", context)
+    except Profile.DoesNotExist:
+        return render(request, "errors/error_404.html", status=404)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
 
 
-def error_404(request, exception):
+def error_404(request, exception): 
     """
     Render a custom 404 error page.
 
     Args:
         request (HttpRequest): The HTTP request object.
         exception (Exception): The exception that triggered the 404 error.
+        Needs exception parameter to avoid SystemCheckError with Sentry
 
     Returns:
         HttpResponse: Rendered 404 error page.
@@ -118,5 +144,8 @@ def simulate_error_500(request):
     Raises:
         Exception: Always raises an exception to simulate a 500 error.
     """
-    raise Exception("Simulating a 500 error!")
-
+    try:
+        raise Exception("Simulating a 500 error!")
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        return render(request, "errors/error_500.html", status=500)
